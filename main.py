e6385674ee2fdd033946f894d85de374345a437d
Daafaadhil <113298157+Daafaadhil@users.noreply.github.com>
2022-09-18 22:16:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+answer = input(' greeting :').lower()
+if 'hello' in answer:
+    print("$0")
+elif 'hello,zakaria' in answer:
+    print("$0")
+elif 'h' in answer:
+    print("$20")
+else:
+    print("$100")
+
+
+
+                 
